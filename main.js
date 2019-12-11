309f77532e611a6b4eecf17a55c8263cbacbe931
Gonzalo Cuadra <gonzalo.cuadra@frogmi.com>
2019-12-11 17:51:17 -0300

    save progress

--- main.js
+++ main.js
@@ -2,22 +2,51 @@
 // nodes
 let input = document.getElementById('input');
 let lista = document.getElementById('lista');
-let todos = {};
+let todos = {
+  0:{
+    text: 'Ir al cine',
+    done: false
+  },
+  1:{
+    text: 'Cenar',
+    done: true
+  },
+  2:{
+    text: 'Grabar',
+    done: false
+  }
+};
 
-// function
+// functions
 function drawTodos(){
   lista.innerHTML = '';
   for (let key in todos) {
     // Crea una etiqueta li
     let li = document.createElement('li');
-    li.id = key;
+    // li.id = key;
     // Agrega dentro de la etiqueta los span con la lista de todos
+    let classDone = todos[key].done ? 'done' : "";
     li.innerHTML = `
-      <span>${todos[key]} </span>
-      <span id="${key}">X</span>
+      <span id="${key}" class="${classDone}">${todos[key].text} </span>
+      <span data-id="${key}" data-action="delete">X</span>
     `;
+    setListeners(li);
     lista.appendChild(li);
   }
+}
+
+function setListeners(li){
+  li.addEventListener('click', (e) => {
+    if (e.target.getAttribute('data-action') === 'delete') {
+      let key = e.target.getAttribute('data-id');
+      delete todos[key];
+      drawTodos();
+      return;
+    }
+    let key = e.target.id;
+    todos[key].done = !todos[key].done;
+    drawTodos();
+  });
 }
 
 // listeners
@@ -25,7 +54,10 @@ input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     let text = e.target.value;
     let id = Object.keys(todos).length;
-    todos[id] = text;
+    todos[id] = {text: text, done:false};
     drawTodos();
   }
 })
+
+//init
+drawTodos();
